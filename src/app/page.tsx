5ed17ff85347394f57a6165755e00a73e3a0f643
NikuKikai <8342552+NikuKikai@users.noreply.github.com>
2025-02-04 05:54:04 +0900

    Add damage to automata with mouse; Custom cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,56 @@ import './globals.css';
 const H = 46;
 const W = 46;
 const CH = 16;
+const DAMAGE_R = 2;
+
+
+function Entry(props: {
+    href: string,
+    target?: React.HTMLAttributeAnchorTarget,
+    children?: React.ReactNode,
+    onEnter?: (x: number, y: number, w: number, h: number) => void,
+    onLeave?: () => void,
+}) {
+    const ref = React.useRef<HTMLSpanElement>(null);
+
+    const onMouseEnter = (e: React.MouseEvent) => {
+        void e;
+        if (!ref.current) return;
+        props.onEnter?.(
+            ref.current.offsetLeft,
+            ref.current.offsetTop,
+            ref.current.offsetWidth,
+            ref.current.offsetHeight,
+        );
+    }
+    const onMouseLeave = (e: React.MouseEvent) => {
+        void e;
+        if (!ref.current) return;
+        props.onLeave?.();
+    }
+
+    return <span ref={ref}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        style={{ marginRight: '1em' }}
+    >
+        <Link href={props.href} target={props.target} style={{ cursor: 'none' }}>
+            {props.children}
+        </Link>
+    </span>
+}
 
 
 export default function Home() {
     const data = React.useRef<Float32Array | null>(null);
+    const damagePos = React.useRef<{ x: number, y: number }>(null);
+    const divRef = React.useRef<HTMLDivElement>(null);
     const [updated, setUpdated] = React.useState<number>(0);
+    const [cursorPos, setCursorPos] = React.useState<{ x: number, y: number }>({ x: -10, y: -10 });
+    const [cursorSize, setCursorSize] = React.useState<{ w: string, h: string }>({ w: '10px', h: '10px' });
+    const [cursorLockPos, setCursorLockPos] = React.useState<{ x: number, y: number }>({ x: -10, y: -10 });
+    const [cursorLockSize, setCursorLockSize] = React.useState<{ w: string, h: string }>({ w: '10px', h: '10px' });
+    const [cursorLocked, setCursorLocked] = React.useState<boolean>(false);
     void updated;
 
     React.useEffect(() => {
@@ -33,17 +78,31 @@ export default function Home() {
             const angle = new ort.Tensor('float64', angleArr, [1]);
 
             intervalHandler = setInterval(async () => {
-                // prepare feeds. use model input names as keys.
-                const feeds = { 'x.1': state, 'angle': angle };
+
 
                 // feed inputs and run
+                const feeds = { 'x.1': state, 'angle': angle };
                 const results = await session.run(feeds);
                 // read from results
                 const stateArr_ = results[89].data as Float32Array;
 
-                // const state_gray = to_gray(stateArr_)
-                // console.log(state_gray);
-                // document.write(`${state_gray}`);
+                // Damage
+                if (damagePos.current) {
+                    const pos = damagePos.current;
+                    // const stateData = await state.getData(true);
+                    for (let x = Math.round(pos.x * W - DAMAGE_R); x < Math.round(pos.x * W + DAMAGE_R); x++) {
+                        if (x < 0 || x >= W) continue;
+                        for (let y = Math.round(pos.y * H - DAMAGE_R); y < Math.round(pos.y * H + DAMAGE_R); y++) {
+                            if (y < 0 || y >= H) continue;
+                            for (let ch = 0; ch < CH; ch++) {
+                                stateArr_[y * W * CH + x * CH + ch] = 0;
+                            }
+                        }
+                    }
+                    // state = new ort.Tensor('float32', stateData, [1, H, W, CH]);
+                    damagePos.current = null;
+                }
+
                 data.current = stateArr_;
                 setUpdated(Date.now());
 
@@ -56,14 +115,52 @@ export default function Home() {
         }
     }, []);
 
-    return <div style={{
+    const onMouseMove = (e: React.MouseEvent) => {
+        // Cursor
+        if (cursorLocked) {
+            setCursorPos(cursorLockPos);
+            setCursorSize(cursorLockSize);
+        }
+        else {
+            setCursorPos({ x: e.clientX, y: e.clientY });
+
+            if (divRef.current)
+                setCursorSize({
+                    w: `${divRef.current.offsetWidth * DAMAGE_R * 2 / W}px`,
+                    h: `${divRef.current.offsetHeight * DAMAGE_R * 2 / H}px`,
+                });
+        }
+
+        // NCA Damage
+        if (divRef.current)
+            damagePos.current = {
+                x: (e.clientX - divRef.current.offsetLeft) / divRef.current.offsetWidth,
+                y: (e.clientY - divRef.current.offsetTop) / divRef.current.offsetHeight,
+            };
+    };
+
+    const onEntryEnter = (x: number, y: number, w: number, h: number) => {
+        setCursorLocked(true);
+        const mx = h / 8;
+        x -= mx;
+        w += mx * 2;
+        setCursorLockPos({ x: x + w / 2, y: y + h / 2 });
+        setCursorLockSize({ w: `${w}px`, h: `${h}px` });
+    }
+
+    const onEntryLeave = () => {
+        setCursorLocked(false);
+    }
+
+    return <div onMouseMove={onMouseMove} style={{
         height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        cursor: 'none',
     }}>
-        <div style={{
+        <div ref={divRef} style={{
             display: 'grid',
             gridTemplateColumns: `repeat(${W}, 1fr)`,
             width: '60vmin', height: '60vmin'
@@ -90,18 +187,29 @@ export default function Home() {
         <div style={{ height: '4em' }} />
         <div style={{ fontSize: '4vmin', fontWeight: 'normal' }}>
             <span style={{ marginRight: '1em', fontWeight: 'bold' }}>MANGA▶</span>
-            <span style={{ marginRight: '1em' }}><Link href='/null1'>NULL1</Link></span>
-            <span></span>
-            <span style={{ marginRight: '1em' }}><Link href='/null2'>NULL2</Link></span>
-            <span style={{ marginRight: '1em' }}><Link href='/Q'>Q</Link></span>
-            <span style={{ marginRight: '1em' }}><Link href='/UglyYuri'>UglyYuri</Link></span>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='/null1'>NULL1</Entry>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='/null2'>NULL2</Entry>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='/Q'>Q</Entry>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='/UglyYuri'>UglyYuri</Entry>
             <div style={{ height: '0' }} />
             <span style={{ marginRight: '1em', fontWeight: 'bold' }}>LINKS▶</span>
-            <span style={{ marginRight: '1em' }}><Link href='https://x.com/NikuKiKai' target='_blank'>X</Link></span>
-            <span style={{ marginRight: '1em' }}><Link href='https://photohito.com/user/159218/' target='_blank'>PHOTO</Link></span>
-            <span style={{ marginRight: '1em' }}><Link href='https://nikukikai.hatenablog.jp/' target='_blank'>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='https://x.com/NikuKiKai' target='_blank'>X</Entry>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='https://photohito.com/user/159218/' target='_blank'>PHOTO</Entry>
+            <Entry onEnter={onEntryEnter} onLeave={onEntryLeave} href='https://nikukikai.hatenablog.jp/' target='_blank'>
                 BLOG<span style={{ fontSize: '1.5vmin' }}>(CHN)</span>
-            </Link></span>
+            </Entry>
         </div>
+
+        {/* Cursor */}
+        <div style={{
+            position: 'absolute',
+            borderWidth: cursorLocked ? '2px' : '1px',
+            borderColor: '#ddd',
+            pointerEvents: 'none',
+            transitionDuration: cursorLocked ? '0.1s' : '0s',
+            transitionTimingFunction: cursorLocked ? 'ease-out' : 'linear',
+            left: `calc(${cursorPos.x}px - ${cursorSize.w} / 2)`, top: `calc(${cursorPos.y}px - ${cursorSize.h} / 2)`,
+            width: `${cursorSize.w}`, height: `${cursorSize.h}`,
+        }} />
     </div>
 }
